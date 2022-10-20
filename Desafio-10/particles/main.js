26bd2a824fe8cd5ea8a5fccf80aea293a47d1f46
Lázaro Ataide de Assis <103513547+LazaroAssis@users.noreply.github.com>
2022-10-20 14:46:44 -0300

    Add files via upload
    Desafio-10 Utilizar Js Lib Particles

---
+++ Desafio-10/particles/main.js
@@ -0,0 +1,3 @@
+// Este conteúdo foi encontrado em https://vincentgarreau.com/particles.js/
+import particles from './particles.json' assert { type: "json" };
+particlesJS("tsparticles", particles);
